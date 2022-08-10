68d88a41d0454bd893ca6c1eec723504bfd5f347
katianaz <katianamzeitz@gmail.com>
2022-08-10 17:35:41 -0300

    envia pacote beans

---
+++ src/java/com/ufpr/tads/web2/beans/CategoriaProduto.java
@@ -0,0 +1,27 @@
+package com.ufpr.tads.web2.beans;
+
+import java.io.Serializable;
+
+
+public class CategoriaProduto implements Serializable {
+    private int idCategoria;
+    private String nome;
+    
+    public CategoriaProduto() {}
+    
+    public CategoriaProduto(String nome){
+        this.nome = nome;
+    }
+    public int getIdCategoria(){
+        return this.idCategoria;
+    }
+    public void setIdCategoria(int idCategoria){
+        this.idCategoria = idCategoria;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
